4d4853bf4345676ce78e2069108879a877f49aff
Antonio Garcia De Paredes Caballero <agarciadug@alumnos.unex.es>
2023-01-11 19:18:33 +0100

    UC 21 - IMP - Test Components - DJ #DONE #time 1m #comments test components

--- app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
+++ app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
@@ -14,7 +14,7 @@ import java.util.TimeZone;
 //Simulación de la implementación del caso de uso 21 - Rol:AS
 //Simulación de la implementación del caso de uso 21 - Rol:DS
 //Simulación de la implementación del caso de uso 21 - Rol:DS
-//Simulación de la implementación del caso de uso 21 - Rol:DS
+//Simulación de la implementación del caso de uso 21 - Rol:DJ
 
 public class DateUtils {
 
